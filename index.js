d37341dacd2ecbcb11e6da0f32d1c6f997c2cc36
Michael Tobia <michaeltobia@gmail.com>
2018-01-17 12:56:08 -0500

    entry:webpack

--- index.js
+++ index.js
@@ -1,49 +1,69 @@
 const debug = process.env.NODE_ENV !== 'production';
 const test = process.env.NODE_ENV === 'test';
 
+const port = process.env.PORT || 3000;
+
 try {
-  if (debug) {
-    if (test === false) {
-      // eslint-disable-next-line import/no-extraneous-dependencies, global-require
-      const webpack = require('webpack');
-      // eslint-disable-next-line import/no-extraneous-dependencies, global-require
-      const koaWebpack = require('koa-webpack');
-      // eslint-disable-next-line import/no-unresolved, global-require
-      const webpackConfig = require('./webpack.config');
-      const compiler = webpack(webpackConfig);
-      const dev = {
-        hot: true,
-        lazy: false,
-        serverSideRender: true,
-        publicPath: `${webpackConfig[0].publicPath}/`,
-        watchOptions: {
-          aggregateTimeout: 800,
-        },
-        noinfo: false,
-        quiet: false,
-        stats: {
-          colors: true,
-        },
-      };
-      const hot = {
-        host: 'localhost',
-        port: 3000,
-      };
-  
-      const koaWebpackMiddleware = koaWebpack({
-        compiler,
-        dev,
-        hot,
+  if (debug === false) {
+    // eslint-disable-next-line global-require
+    const app = require('./dist/app');
+
+    app.listen(port);
+  } else if (test === false) {
+    let app = (req, res) => res.end();
+
+    // eslint-disable-next-line global-require
+    const server = require('http')
+      .createServer((req, res) => app(req, res));
+
+    server
+      .listen(port, (error) => {
+        if (error) throw error;
+
+        const serve = () => {
+          // eslint-disable-next-line global-require
+          app = require('./dist/app').callback();
+        };
+
+        // eslint-disable-next-line import/no-extraneous-dependencies, global-require
+        const webpack = require('webpack');
+        // eslint-disable-next-line import/no-extraneous-dependencies, global-require
+        const koaWebpack = require('koa-webpack');
+        // eslint-disable-next-line global-require
+        const webpackConfig = require('./webpack.config');
+
+        const compiler = webpack(webpackConfig);
+
+        const dev = {
+          hot: true,
+          lazy: false,
+          serverSideRender: true,
+          publicPath: `${webpackConfig[0].publicPath}/`,
+          watchOptions: {
+            aggregateTimeout: 800,
+          },
+          noinfo: false,
+          quiet: false,
+          stats: {
+            colors: true,
+          },
+        };
+
+        const hot = {
+          server,
+        };
+
+        const koaWebpackMiddleware = koaWebpack({
+          compiler,
+          dev,
+          hot,
+        });
+
+        global.webpack = koaWebpackMiddleware;
+
+        serve();
       });
-  
-      global.webpack = koaWebpackMiddleware;
-    }
   }
-
-  // eslint-disable-next-line global-require
-  const app = require('./dist/app');
-
-  app.listen(process.env.PORT || 3000);
 } catch (e) {
   if (e.code === 'MODULE_NOT_FOUND') console.log('please run "npm run build" before you begin.\n');
   else throw e;

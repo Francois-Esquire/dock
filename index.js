8d6f7fee48207e6ab3f8ce98c98c285180160a2e
Michael Tobia <michaeltobia@gmail.com>
2018-02-04 16:13:04 -0500

    moved dev setup from index.js

--- index.js
+++ index.js
@@ -18,48 +18,10 @@ try {
     server.listen(port, error => {
       if (error) throw error;
 
-      const serve = () => {
-        // eslint-disable-next-line global-require
-        app = require('./dist/app').callback();
-      };
-
-      // eslint-disable-next-line import/no-extraneous-dependencies, global-require
-      const webpack = require('webpack');
-      // eslint-disable-next-line import/no-extraneous-dependencies, global-require
-      const koaWebpack = require('koa-webpack');
       // eslint-disable-next-line global-require
-      const webpackConfig = require('./webpack.config');
-
-      const compiler = webpack(webpackConfig);
-
-      const dev = {
-        hot: true,
-        lazy: false,
-        serverSideRender: true,
-        publicPath: `${webpackConfig[0].publicPath}/`,
-        watchOptions: {
-          aggregateTimeout: 800,
-        },
-        noinfo: false,
-        quiet: false,
-        stats: {
-          colors: true,
-        },
-      };
-
-      const hot = {
-        server,
-      };
-
-      const koaWebpackMiddleware = koaWebpack({
-        compiler,
-        dev,
-        hot,
+      require('./src/dev')(server, koa => {
+        app = koa;
       });
-
-      global.webpack = koaWebpackMiddleware;
-
-      serve();
     });
   }
 } catch (e) {

5764200e186c73472fe11c179e8bc06b62b75104
Michael Tobia <michaeltobia@gmail.com>
2018-04-01 17:21:05 -0400

    added main styles to build

--- src/www/main.jsx
+++ src/www/main.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 
+import '../styles/main.scss';
+
 import Application from './Application';
 
 const supportsHistory = 'pushState' in window.history;

7eb26ba1e1254cc54ecb7abac89eca6815d97516
Michael Tobia <michaeltobia@gmail.com>
2018-04-19 06:00:49 -0400

    added config

---
+++ config.js
@@ -0,0 +1,12 @@
+const protocol = 'http';
+const domain = 'localhost';
+const port = 3000;
+
+const host = `${protocol}://${domain}:${port}`;
+
+module.exports = {
+  protocol,
+  domain,
+  port,
+  host,
+};

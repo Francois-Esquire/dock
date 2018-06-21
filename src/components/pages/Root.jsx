b18f6fe9187e35ada39e62fa2dc493c30ff5ddfa
Michael Tobia <michaeltobia@gmail.com>
2018-06-21 19:52:46 -0400

    added 404 to root router

--- src/components/pages/Root.jsx
+++ src/components/pages/Root.jsx
@@ -1,15 +1,24 @@
 import React from 'react';
-
 import { withRouter, Switch, Route } from 'react-router-dom';
 
 import Home from './Home';
+import NotFound from './404';
 
-function Router() {
-  return (
-    <Switch>
-      <Route component={Home} />
-    </Switch>
-  );
+class Router extends React.PureComponent {
+  // eslint-disable-next-line class-methods-use-this
+  componentDidCatch(error, errorInfo) {
+    console.log(error, errorInfo);
+  }
+
+  render() {
+    return (
+      <Switch>
+        <Route exact path="/" component={Home} />
+
+        <Route component={NotFound} />
+      </Switch>
+    );
+  }
 }
 
 const Root = withRouter(Router);

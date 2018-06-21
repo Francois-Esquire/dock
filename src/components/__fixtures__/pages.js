21d4d5a9fe2d2d99c006a87a03201e7541b902c3
Michael Tobia <michaeltobia@gmail.com>
2018-06-21 19:52:17 -0400

    added fixture for 404 page

--- src/components/__fixtures__/pages.js
+++ src/components/__fixtures__/pages.js
@@ -1,9 +1,15 @@
 import Home from '../pages/Home';
+import NotFound from '../pages/404';
 
 export default [
   {
     name: 'Home Page',
     component: Home,
     url: '/',
+  },
+  {
+    name: '404 Page',
+    component: NotFound,
+    url: '/404',
   },
 ];

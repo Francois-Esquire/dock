003bb570645ed0c51fd68d82f097f12fc3692f13
Michael Tobia <michaeltobia@gmail.com>
2018-06-21 19:55:41 -0400

    modified deploy script | package.json support for different tags

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -2,8 +2,12 @@ import { exec } from './helpers';
 
 import pkg from '../package.json';
 
-const { image, registry } = pkg.deploy;
+const { image, registry, tags } = pkg.deploy;
 
 exec(`docker build -t ${image} .`);
-exec(`docker tag ${image} ${registry}/${image}`);
+
+(tags || []).forEach(
+  tag => tag && exec(`docker tag ${image} ${registry}/${image}:${tag}`),
+);
+
 exec(`docker push ${registry}/${image}`);
